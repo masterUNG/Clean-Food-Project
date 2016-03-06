cd4e870eba2f0d65c565e10017b907eb991b0100
masterUNG <phrombutr@gmail.com>
2016-03-06 13:18:28 +0700

    Create ListView Finish

--- app/src/main/java/suthasidev/cleanfoodproject/MyAdapter.java
+++ app/src/main/java/suthasidev/cleanfoodproject/MyAdapter.java
@@ -1,7 +1,59 @@
 package suthasidev.cleanfoodproject;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
 /**
  * Created by masterUNG on 3/6/16 AD.
  */
-public class MyAdapter {
-}
+public class MyAdapter extends BaseAdapter{
+
+    //Explicit
+    private Context context;
+    private String[] iconStrings, titleStrings;
+
+    public MyAdapter(Context context, String[] iconStrings, String[] titleStrings) {
+        this.context = context;
+        this.iconStrings = iconStrings;
+        this.titleStrings = titleStrings;
+    }   // Constructor
+
+    @Override
+    public int getCount() {
+        return iconStrings.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view1 = layoutInflater.inflate(R.layout.recipe_listview, viewGroup, false);
+
+        //For Image
+        ImageView iconImageView = (ImageView) view1.findViewById(R.id.imageView);
+        Picasso.with(context).load(iconStrings[i]).resize(100, 100).into(iconImageView);
+
+        //For TextView
+        TextView titleTextView = (TextView) view1.findViewById(R.id.textView2);
+        titleTextView.setText(titleStrings[i]);
+
+        return view1;
+    }
+}   // Main Class
--- app/src/main/java/suthasidev/cleanfoodproject/ServiceActivity.java
+++ app/src/main/java/suthasidev/cleanfoodproject/ServiceActivity.java
@@ -1,5 +1,7 @@
 package suthasidev.cleanfoodproject;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
@@ -23,8 +25,45 @@ public class ServiceActivity extends AppCompatActivity {
         //Show Name
         showName();
 
+        //Create ListView
+        createListView();
+
 
     }   // Main Method
+
+    private void createListView() {
+
+        //Read All SQLite
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE, null);
+        Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM " + MyManage.table_recipe, null);
+        cursor.moveToFirst();
+
+        int intCount = cursor.getCount();
+        String[] recipeStrings = new String[intCount];
+        String[] ingredientsStrings = new String[intCount];
+        String[] howToStrings = new String[intCount];
+        String[] descriptionStrings = new String[intCount];
+        String[] imageRecipeStrings = new String[intCount];
+
+        for (int i=0;i<intCount;i++) {
+
+            recipeStrings[i] = cursor.getString(cursor.getColumnIndex(MyManage.column_Recipe));
+            ingredientsStrings[i] = cursor.getString(cursor.getColumnIndex(MyManage.column_Ingredients));
+            howToStrings[i] = cursor.getString(cursor.getColumnIndex(MyManage.column_HowTo));
+            descriptionStrings[i] = cursor.getString(cursor.getColumnIndex(MyManage.column_Description));
+            imageRecipeStrings[i] = cursor.getString(cursor.getColumnIndex(MyManage.column_ImageRecipe));
+
+            cursor.moveToNext();
+        }   // for
+        cursor.close();
+
+        MyAdapter myAdapter = new MyAdapter(ServiceActivity.this, imageRecipeStrings, recipeStrings);
+        recipeListView.setAdapter(myAdapter);
+
+
+
+    }   // createListView
 
     private void showName() {
 
