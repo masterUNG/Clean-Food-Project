b6df4bb4ada634520a785a050e827c1343507ef3
masterUNG <phrombutr@gmail.com>
2016-03-06 10:21:11 +0700

    Intent to SignIn finish1

--- app/src/main/java/suthasidev/cleanfoodproject/SignInActivity.java
+++ app/src/main/java/suthasidev/cleanfoodproject/SignInActivity.java
@@ -2,12 +2,32 @@ package suthasidev.cleanfoodproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SignInActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText userEditText, passwordEditText;
+    private String userString, passwordString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        //Bind Widget
+        bindWidget();
+
+    }   // Main Method
+
+    private void bindWidget() {
+        userEditText = (EditText) findViewById(R.id.editText);
+        passwordEditText = (EditText) findViewById(R.id.editText2);
     }
-}
+
+    public void clickSignInAuthen(View view) {
+
+    }   // clickSignInAuthen
+
+}   // Main Class
